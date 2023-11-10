248c79b4111b333b6db01a40f75a52988829d7ca
Maxim Leontyev <leontyev.maxim@mail.ru>
2023-11-11 01:42:46 +0300

    поел. отрицательное количество циклов (эмодзи 'палец вверх')

--- graph.py
+++ graph.py
@@ -1,18 +1,17 @@
 class Graph:
     def __init__(self):
-        self.vertices = []
+        self.vertexes = []
         self.adjacency_list = {}
 
     def print_adjacency_list(self):
-        if not self.vertices:
+        if not self.vertexes:
             return []
         result = {}
-        for vertex in self.vertices:
+        for vertex in self.vertexes:
             result[vertex] = []
             for edge in self.adjacency_list[vertex]:
                 result[vertex].append(edge[0])
-        print("Список смежности")
-        print("Вершина: смежная вершина")
+        print("Список смежности\nвершина: смежная вершина")
         for key, value in result.items():
             print(f"{key}: {', '.join(value)}")
 
@@ -36,52 +35,52 @@ class Graph:
         return list(self.adjacency_list[v])[i]
 
     def add_v(self, name, label=None, mark=None):
-        if name in self.vertices:
+        if name in self.vertexes:
             raise ValueError(f"Вершина с именем {name} уже существует")
-        self.vertices.append(name)
+        self.vertexes.append(name)
         self.adjacency_list[name] = []
         if label is not None:
-            self.vertices[-1] = label
+            self.vertexes[-1] = label
         if mark is not None:
-            self.vertices[-1] = mark
+            self.vertexes[-1] = mark
 
     def add_e(self, v1, v2, c):
-        if v1 not in self.vertices:
+        if v1 not in self.vertexes:
             raise ValueError(f"Вершина {v1} не существует")
-        if v2 not in self.vertices:
+        if v2 not in self.vertexes:
             raise ValueError(f"Вершина {v2} не существует")
         self.adjacency_list[v1].append((v2, c))
         self.adjacency_list[v2].append((v1, c))
 
     def del_v(self, v):
-        if v not in self.vertices:
+        if v not in self.vertexes:
             raise ValueError(f"Вершина {v} не существует")
-        for vertex in self.vertices:
+        for vertex in self.vertexes:
             if v in self.adjacency_list[vertex]:
                 self.adjacency_list[vertex].remove(v)
-        del self.vertices[self.vertices.index(v)]
+        del self.vertexes[self.vertexes.index(v)]
         del self.adjacency_list[v]
 
     def del_e(self, v1, v2):
-        if v1 not in self.vertices:
+        if v1 not in self.vertexes:
             raise ValueError(f"Вершина {v1} не существует")
-        if v2 not in self.vertices:
+        if v2 not in self.vertexes:
             raise ValueError(f"Вершина {v2} не существует")
         self.adjacency_list[v1].remove((v2, None))
         self.adjacency_list[v2].remove((v1, None))
 
     def edit_v(self, v, new_label=None, new_mark=None):
-        if v not in self.vertices:
+        if v not in self.vertexes:
             raise ValueError(f"Вершина {v} не существует")
         if new_label is not None:
-            self.vertices[self.vertices.index(v)] = new_label
+            self.vertexes[self.vertexes.index(v)] = new_label
         if new_mark is not None:
-            self.vertices[self.vertices.index(v)] = new_mark
+            self.vertexes[self.vertexes.index(v)] = new_mark
 
     def edit_e(self, v1, v2, new_c):
-        if v1 not in self.vertices:
+        if v1 not in self.vertexes:
             raise ValueError(f"Вершина {v1} не существует")
-        if v2 not in self.vertices:
+        if v2 not in self.vertexes:
             raise ValueError(f"Вершина {v2} не существует")
         for edge in self.adjacency_list[v1]:
             if edge[0] == v2:
@@ -91,4 +90,3 @@ class Graph:
             if edge[0] == v1:
                 edge[1] = new_c
                 break
-
--- main.py
+++ main.py
@@ -27,28 +27,42 @@ from graph import Graph
 #
 # ]
 
+# VERTEXES_NAMES: list[str] = [
+#     "A",
+#     "B",
+#     "C",
+#     "D",
+#     "E",
+#     "F",
+#     "G",
+#     "H",
+# ]
+# EDGES: list[list[str, str, int]] = [
+#     ["A", "D", 1],
+#     ["A", "E", 1],
+#     ["B", "D", 1],
+#     ["B", "C", 1],
+#     ["C", "G", 1],
+#     ["D", "E", 1],
+#     ["E", "H", 1],
+#     ["E", "F", 1],
+#     ["F", "G", 1],
+#     ["G", "H", 1],
+#
+# ]
 VERTEXES_NAMES: list[str] = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H"
+    "0",
+    "1",
+    "2",
+    "3",
+    "4"
 ]
 EDGES: list[list[str, str, int]] = [
-    ["A", "D", 1],
-    ["A", "E", 1],
-    ["B", "D", 1],
-    ["B", "C", 1],
-    ["C", "G", 1],
-    ["D", "E", 1],
-    ["E", "H", 1],
-    ["E", "F", 1],
-    ["F", "G", 1],
-    ["G", "H", 1],
-
+    ["0", "1", 1],
+    ["0", "2", 1],
+    ["1", "3", 1],
+    ["2", "3", 1],
+    ["3", "4", 1]
 ]
 
 def __init_graph():
@@ -60,24 +74,45 @@ def __init_graph():
     return g
 
 
-def cyclomatic_complexity_dfs(graph1):
-    # Инициализация переменных.
-    cc = []
-    visited = set()
+# def cyclomatic_complexity_dfs(graph1):
+#     # Инициализация переменных.
+#     cc = []
+#     visited = set()
+#
+#     def dfs(vertex):
+#         visited.add(vertex)
+#         for neighbor, _ in graph1.adjacency_list[vertex]:
+#             if neighbor not in visited:
+#                 dfs(neighbor)
+#         if graph1.adjacency_list[vertex]:
+#             cc.append("1")
+#
+#     dfs(graph1.vertexes[0])
+#     return len(cc)
+def dfs(graph1: Graph, start: str, visited: dict[str, bool]):
+    visited[start] = True
 
-    def dfs(vertex):
-        visited.add(vertex)
-        for neighbor, _ in graph1.adjacency_list[vertex]:
-            if neighbor not in visited:
-                dfs(neighbor)
-        if graph1.adjacency_list[vertex]:
-            cc.append("1")
+    for neighbor, _ in graph1.adjacency_list[start]:
+        if not visited[neighbor]:
+            dfs(graph1, neighbor, visited)
 
-    dfs(graph1.vertices[0])
-    return len(cc)
+
+def calculate_cyclomatic_complexity(graph1):
+    visited = {}
+    for vertex in graph1.vertexes:
+        visited[vertex] = False
+    count_edges = 0
+
+    for vertex in graph1.adjacency_list:
+        print(vertex)
+        if not visited[vertex]:
+            dfs(graph1, vertex, visited)
+            count_edges += 1
+
+    return count_edges - len(graph1.adjacency_list) + 2
 
 
 if __name__ == '__main__':
     graph = __init_graph()
     graph.print_adjacency_list()
-    print(f"Цикломатическая сложность графа - {cyclomatic_complexity_dfs(graph)}")
+    print(f"Цикломатическая сложность графа - {calculate_cyclomatic_complexity(graph)}")
